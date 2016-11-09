49dd48c5c15f6f50bad0fd841453d3e59e89b1ed
lucasbegnini <lucasbegnini@gmail.com>
2016-11-09 10:45:31 -0200

    [XMLDOWNLOAD] Clear

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main(int argc, char **argv)
 {
  QApplication app (argc, argv);
  Window window;
- //window.show();
+
  return app.exec();
 }
--- xmldown.cpp
+++ xmldown.cpp
@@ -10,11 +10,7 @@ Xmldown::Xmldown(QByteArray datain, QString* tempurl)
 {
     xml.setContent(datain);
     _tempurl = *tempurl;
-   // QDomNodeList list=xml.elementsByTagName(QString("QualityLevel"));
-   // QString DataAsString = QTextCodec::codecForMib(1015)->toUnicode(datain);
-    // qDebug(DataAsString.toLatin1());
 
-     //qDebug()<<list.at(0).toElement().attribute("Index");
     //Take QualityLevels
      qtdQualityLevels = xml.elementsByTagName(QString("StreamIndex")).at(0).toElement().attribute("QualityLevels").toInt();
      //Take type
@@ -24,18 +20,11 @@ Xmldown::Xmldown(QByteArray datain, QString* tempurl)
       //Take URL
       _URL = xml.elementsByTagName(QString("StreamIndex")).at(0).toElement().attribute("Url");
     //Gera uma lista so com os videos onde cada .at(i) é um QualityLevel
-
     QDomNodeList list=xml.elementsByTagName(QString("StreamIndex")).at(0).childNodes();
     QDomNodeList listchunks =xml.elementsByTagName(QString("c"));
-    // qDebug()<< qtdQualityLevels;
-    // qDebug()<< Type;
-    // qDebug()<< chunks;
-    // qDebug()<< _tempurl;
 
+    //Call SmoothStreaming and start.
     SmoothStreaming download;
     download.SetTudo(listchunks, list , Type, qtdQualityLevels,chunks,_URL, _tempurl);
-       //if(list.at(0).toElement().attribute("Index"))
-      //Lista todas Quality Level de vídeo.
-      //QDomNodeList QualityLevelVideoList = list.at(0).elementsByTagName(QString("QualityLevel"));
-       //qDebug()<<list.at(0).toElement().attribute("Bitrate");
+
 }
